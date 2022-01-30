5cd6a9e78e03d5d13ba37e016f215cd108594199
Raven K <85319728+ravenkmusic@users.noreply.github.com>
2022-01-30 15:14:27 +0000

    added dictionary of operations for the user to choose from

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 #Calculator
 
-operations
 def add (n1, n2):
   return n1 + n2
  
@@ -13,6 +12,10 @@ def multiply (n1, n2):
 def divide (n1, n2):
   return n1 / n2
 
+operations = {"+": add, "-": subtract, "*": multiply, "/": divide}
+
 first_number = int(input("Enter your first number.\n"))
-operation = input("Select your operator between +, -, *, or /. \n")
 second_number = int(input("Enter your second number.\n"))
+
+for key in operations:
+  key = input("Select your operator between +, -, *, or /. \n")
