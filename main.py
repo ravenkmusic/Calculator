44f9295c274e3fb8590ea5d690dbcd57ebc527df
Raven K <85319728+ravenkmusic@users.noreply.github.com>
2022-01-30 17:11:24 +0000

    added while loop so user can do as many calculations as they please

--- main.py
+++ main.py
@@ -22,13 +22,27 @@ operations = {
 first_number = int(input("Enter your first number.\n"))
 for symbol in operations:
   print(symbol)
-operations_symbol = input("Select your operator from the four choices displayed above. \n")
-second_number = int(input("Enter your second number.\n"))
 
-calculate = operations[operations_symbol]
-#by using the variable "calculate", we are accessing the VALUE, not the key of the operations symbol that the user selected.
-answer = calculate(first_number, second_number)
+calculation_continues = True
 
-#we've turned the variable into one of the already defined functions as a result. calculate() = add(first_number, second_number), subtract(first_number, second_number), etc, etc.
+while calculation_continues:
+  operations_symbol = input("Select your operator from the four choices displayed above. \n")
+  second_number = int(input("Enter your next number.\n"))
 
-print(f"{first_number} {operations_symbol} {second_number} = {answer}")
+  calculate = operations[operations_symbol]
+  #by using the variable "calculate", we are accessing the VALUE, not the key of the operations symbol that the user selected.
+  answer = calculate(first_number, second_number)
+
+  #we've turned the variable into one of the already defined functions as a result. calculate() = add(first_number, second_number), subtract(first_number, second_number), etc, etc.
+
+  keep_operating = input(f"Would you like to keep calculating with {answer}? Type 'y' to continue or 'n' to exit the program.\n").lower
+
+  if keep_operating == "y":
+    calculation_continues = True
+    first_number = answer
+    print(f"Let's continue with {first_number}.")
+  else:
+    calculation_continues = False
+    print("Bye!")
+
+calculator()
