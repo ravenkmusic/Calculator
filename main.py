de4a861354a3902c46078695e326a3b458756436
RavenKMusic <85319728+ravenkmusic@users.noreply.github.com>
2022-01-30 15:06:00 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+#Calculator
+
+operations
+def add (n1, n2):
+  return n1 + n2
+ 
+def subtract (n1, n2):
+  return n1 - n2
+
+def multiply (n1, n2):
+  return n1 * n2
+
+def divide (n1, n2):
+  return n1 / n2
+
+first_number = int(input("Enter your first number.\n"))
+operation = input("Select your operator between +, -, *, or /. \n")
+second_number = int(input("Enter your second number.\n"))
